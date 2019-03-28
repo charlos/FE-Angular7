6bc8fb6de74b3f3f43a3606953cce6d2105497b5
cflores <cflores@finnegans.com.ar>
2019-03-28 13:32:57 -0300

    Se agrega await para listar los Legajos

--- src/app/legajo/legajo/legajo.component.ts
+++ src/app/legajo/legajo/legajo.component.ts
@@ -13,52 +13,31 @@ export class LegajoComponent implements AfterViewInit {
   displayedColumns: string[] = ['ID', 'Creado', 'Nombre', 'Legajo'];
   exampleDatabase: LegajoDB | null;
   data: LegajosItem[] = [];
+  //data: LegajosApi;
 
   resultsLength = 0;
   isLoadingResults = true;
   isRateLimitReached = false;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private http: HttpClient) { }
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
     this.exampleDatabase = new LegajoDB(this.http);
 
-    // If the user changes the sort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => this.paginator.pageIndex = 0);
+    //this.isLoadingResults = false;
+    const legajosApi: LegajosApi = await this.exampleDatabase.getLegajos(this.sort.active, this.sort.direction, 1);
+    this.data = legajosApi.items;
+    this.isLoadingResults = false;
 
-    merge(this.sort.sortChange, this.paginator.page)
-      .pipe(
-        startWith({}),
-        switchMap(() => {
-          this.isLoadingResults = true;
-          return this.exampleDatabase!.getLegajos(
-            this.sort.active, this.sort.direction, this.paginator.pageIndex);
-        }),
-        map(data => {
-          // Flip flag to show that loading has finished.
-          this.isLoadingResults = false;
-          this.isRateLimitReached = false;
-          this.resultsLength = 1; // deberia ir algo del estilo "data.total_count"
-
-          return data; // deberia ir algo del estilo "data.items"
-        }),
-        catchError(() => {
-          this.isLoadingResults = false;
-          // Catch if the GitHub API has reached its rate limit. Return empty data.
-          this.isRateLimitReached = true;
-          return observableOf([]);
-        })
-      ).subscribe(data => this.data = data);
   }
 }
 
-/*export interface LegajosApi {
+export interface LegajosApi {
   items: LegajosItem[];
   total_count: number;
-}*/
+}
 
 export interface LegajosItem {
   ID: number;
@@ -75,19 +54,23 @@ export interface LegajosItem {
   Direccion: string;
 };
 
-export interface LegajosApi extends Array<LegajosItem> {};
+//export interface LegajosApi extends Array<LegajosItem> {};
 
 export class LegajoDB {
   constructor(private http: HttpClient) { }
 
-  getLegajos(sort: string, order: string, page: number): Observable<LegajosApi> {
+  public async getLegajos(sort: string, order: string, page: number): Promise<LegajosApi> {
     const token = JSON.parse(localStorage.getItem('currentUser')).Token
     const headers = new HttpHeaders()
       .append('token', token)
     const href = '/api/legajo/legajos';
     const requestUrl =
-      `${href}?q=repo:angular/material2&sort=${sort}&order=${order}&page=${page + 1}`;
+      `${href}`;
 
-    return this.http.get<LegajosApi>(requestUrl, {headers});
+    let legajosApi: LegajosApi = { items: null, total_count: null };
+    legajosApi.items = await this.http.get<LegajosItem[]>(requestUrl, {headers}).toPromise();
+    legajosApi.total_count = legajosApi.items.length;
+    
+    return legajosApi;
   }
 };
